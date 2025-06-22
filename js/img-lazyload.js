c7cc6e90bfc99955a1ecae8038e01e483fdb9b48
fulequn <2382019442@qq.com>
2025-06-22 11:22:22 +0800

    Site updated: 2025-06-22 11:22:19

--- js/img-lazyload.js
+++ js/img-lazyload.js
@@ -1,1 +1,1 @@
-!function(){for(const t of document.querySelectorAll("img[lazyload]"))Fluid.utils.waitElementVisible(t,function(){t.removeAttribute("srcset"),t.removeAttribute("lazyload")},CONFIG.lazyload.offset_factor)}(window);
+(()=>{for(let e of document.querySelectorAll("img[lazyload]"))Fluid.utils.waitElementVisible(e,function(){e.removeAttribute("srcset"),e.removeAttribute("lazyload")},CONFIG.lazyload.offset_factor)})(window);
